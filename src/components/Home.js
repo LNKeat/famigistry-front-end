e2dd731df1332f1a7c722ee970b73c2e628fe6d6
Laura <lnkeat@gmail.com>
2021-10-26 12:59:24 -0600

    worked on home page css

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,12 +5,11 @@ function Home() {
   return (
     <main>
       <div className="home-info">
-        <div>
-          Welcome to this registry for creating and searching wish lists
-          uniquely designed for your family and friends.
-        </div>
+         <p>
+            Welcome to this registry for creating and searching wish lists
+            uniquely designed for your family and friends.
+        </p>
       </div>
-      {/* <img src={family} alt="" /> */}
     </main>
   )
 }
